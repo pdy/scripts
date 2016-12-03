73c20bbb4c7ad14293e6c526848e3b00d473cb04
severalgh <pdrzycimski@gmail.com>
2016-12-03 08:58:09 -0500

    Modify README with basic info and add first three scripts

---
+++ find_and_replace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+toBeReplaced=$1
+newStr=$2
+dir='./'
+
+if [ $# -eq 3 ]
+    then
+       dir=$3 
+fi
+
+echo "Replacing [$toBeReplaced] to [$newStr] in dir [$dir]"
+
+grep -rl $toBeReplaced $dir | xargs sed -i "s/$toBeReplaced/$newStr/g"
---
+++ vim_from_src.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+
+
+apt-get remove vim vim-runtime gvim
+
+apt-get remove vim-tiny vim-common vim-gui-common vim-nox
+
+apt-get autoremove
+
+cd ~
+git clone https://github.com/vim/vim.git
+cd vim
+
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.5/config-3.4m-x86_64-linux-gnu \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+
+make VIMRUNTIMEDIR=/usr/share/vim/vim80
+
+cd ~/vim
+make install
+
+update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+update-alternatives --set editor /usr/bin/vim
+update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+update-alternatives --set vi /usr/bin/vim
+
+
+
+
+
