476ba9ad29a4c88a8af1ad68e1005519396970f7
severalgh <pdrzycimski@gmail.com>
2017-05-03 21:12:30 +0200

    Remove valgrind and nikto from installed packages

--- new_debian_helpers/debian_1_step.sh
+++ new_debian_helpers/debian_1_step.sh
@@ -14,7 +14,7 @@ mkdir repo
 mkdir repo/github
 cd repo/github
 
-sudo apt-get -qq install git git-core htop clang gdb build-essential cmake valgrid nikto nmap cloc &&
+sudo apt-get -qq install git git-core htop clang gdb build-essential cmake nmap cloc &&
 
 git clone https://github.com/severalgh/scripts.git &&
 
