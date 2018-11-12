5c08f2650a73390b0255c786c6006d575c1eebb5
Pawel Drzycimski <pdrzycimski@gmail.com>
2018-11-12 20:08:34 +0100

    Add installing scripts utils when creating new machine

--- new_debian_helpers/debian_1_step.sh
+++ new_debian_helpers/debian_1_step.sh
@@ -13,14 +13,16 @@ cd ~/
 mkdir -p repo/github
 cd repo/github
 
+# install what is essential
 sudo apt-get install git git-core htop clang gdb build-essential cmake nmap cloc linux-tools &&
 
+# install my custom stuff
 git clone https://github.com/pdy/scripts.git &&
-
 git clone https://github.com/pdy/dotfiles.git &&
 
 sudo ./scripts/new_debian_helpers/guest_additions.sh
 ./scripts/vim/vim_from_src.sh
+sudo ./repo/github/scripts/install.sh
 
 cd ~/
 ./repo/github/scripts/vim/vundle_checkout.sh
