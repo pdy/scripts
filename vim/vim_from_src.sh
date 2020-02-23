d1b84ce75ea3fbbd0ab4d074a39478a11adc7c91
Pawel Drzycimski <pdrzycimski@gmail.com>
2020-02-23 21:19:38 +0100

    Update vim installer script to newer version.

--- vim/vim_from_src.sh
+++ vim/vim_from_src.sh
@@ -9,11 +9,10 @@ if [ "$(id -u)" == "0" ]; then
     exit 1
 fi
 
-sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
-
+    python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
 
 sudo apt-get remove vim vim-runtime gvim
 
@@ -28,25 +27,24 @@ cd vim
 ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ \
             --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config-3.4m-x86_64-linux-gnu \
+            --with-python3-config-dir=$(python3-config --configdir) \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
-            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+            --enable-gui=gtk2 \
+            --enable-cscope \
+            --prefix=/usr/local
 
-make VIMRUNTIMEDIR=/usr/share/vim/vim80
+
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim82
 
 cd ~/vim
 sudo make install
 
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-sudo update-alternatives --set editor /usr/bin/vim
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-sudo update-alternatives --set vi /usr/bin/vim
-
-
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
+sudo update-alternatives --set editor /usr/local/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
+sudo update-alternatives --set vi /usr/local/bin/vim
 
 
 
