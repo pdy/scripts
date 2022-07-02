9475bba524ad068fa5a3217e4572e92bbb05ab4c
pdy <pdrzycimski@gmail.com>
2022-07-02 23:03:03 +0200

    Update vim installer script

--- vim/vim_from_src.sh
+++ vim/vim_from_src.sh
@@ -9,16 +9,15 @@ if [ "$(id -u)" == "0" ]; then
     exit 1
 fi
 
-sudo apt install libncurses5-dev libgtk-3-dev \
-    libgtk2.0-dev libatk1.0-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+sudo apt install libncurses5-dev libgtk-3-dev libgtk2.0-dev libatk1.0-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python2-dev \
     python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
 
-sudo apt-get remove vim vim-runtime gvim
+sudo apt remove vim vim-runtime gvim
 
-sudo apt-get remove vim-tiny vim-common vim-gui-common vim-nox
+sudo apt remove vim-tiny vim-common vim-gui-common vim-nox
 
-sudo apt-get autoremove
+sudo apt autoremove
 
 cd ~
 git clone https://github.com/vim/vim.git
@@ -36,7 +35,7 @@ cd vim
             --prefix=/usr/local
 
 
-make VIMRUNTIMEDIR=/usr/local/share/vim/vim82
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim90
 
 cd ~/vim
 sudo make install
@@ -45,6 +44,3 @@ sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim 1
 sudo update-alternatives --set editor /usr/local/bin/vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 1
 sudo update-alternatives --set vi /usr/local/bin/vim
-
-
-
