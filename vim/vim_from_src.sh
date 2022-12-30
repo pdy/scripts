5d4413bf04b0f0e318518d9c2db929c3b6483d8f
pdy <pdrzycimski@gmail.com>
2022-12-30 18:21:49 +0100

    Remove python2-dev installation

--- vim/vim_from_src.sh
+++ vim/vim_from_src.sh
@@ -10,7 +10,7 @@ if [ "$(id -u)" == "0" ]; then
 fi
 
 sudo apt install libncurses5-dev libgtk-3-dev libgtk2.0-dev libatk1.0-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev \
     python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
 
 sudo apt remove vim vim-runtime gvim
