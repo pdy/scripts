7e59089866463227564c5dc85767900ad7e2cc61
pdy <pdrzycimski@gmail.com>
2025-03-30 17:31:15 +0200

    Add some ergonomy to set_local_gh.sh scritp

--- utils/set_local_gh.sh
+++ utils/set_local_gh.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "Require one argument - path to git repository to set local user name and email."
+user="pdy"
+email="pdrzycimski@gmail.com"
+
+script_name=$(basename "$0")
+
+USAGE="Usage ${script_name} <git_repo_path> (required) <user_name> (optional default \"${user}\") <user_email> (optional default \"${email}\")"
+
+if [ $# -eq 0 ]; then
+  echo ""
+  echo "Path to git repo required as a first argument !!!"
+  echo $USAGE
+  echo ""
+  exit 1
+fi
+
+if [ $# -gt 3 ]; then
+  echo ""
+  echo "Too many arguments !!!"
+  echo $USAGE
+  echo ""
   exit 1
 fi
 
 path=$1
 
-user="pdy"
-email="pdrzycimski@gmail.com"
+if [ $# -eq 2 ]; then
+  user=$2
+fi
+
+if [ $# -eq 3 ]; then
+  user=$2
+  email=$3
+fi
 
 pushd $path &&
 
-echo "Changing local user.name to ${user} in ${path}"
+echo ""
+echo "Changing local user.name to \"${user}\" in ${path}"
 
-git config --local user.name $user &&
+git config --local user.name "${user}" &&
 
-echo "Changing local user.email to ${email} in ${path}"
+echo "Changing local user.email to \"${email}\" in ${path}"
+echo ""
 
-git config --local user.email $email 
+git config --local user.email "${email}"
 
 popd
