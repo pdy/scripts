2f4a8cae6d2dd8085a4e70cf74c54839a74a52cd
pdy <pdrzycimski@gmail.com>
2025-04-09 01:12:21 +0200

    add script to create make profiler pythong venv

--- utils/create_make_profiler.sh
+++ utils/create_make_profiler.sh
@@ -7,7 +7,7 @@ fi
 
 #sudo apt install python3-venv
 
-name=$1
+name = $1
 
 python3 -m venv $name 
-$name/bin/pip3 install https://github.com/gojuno/make-profiler/archive/master.zip
+$name install https://github.com/gojuno/make-profiler/archive/master.zip
