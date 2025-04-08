11d1df41c16ff0382f755cfac7f0b57a14548ff3
pdy <pdrzycimski@gmail.com>
2025-04-09 01:13:32 +0200

    Revert "add script to create make profiler pythong venv"
    This reverts commit 2f4a8cae6d2dd8085a4e70cf74c54839a74a52cd.

--- utils/create_make_profiler.sh
+++ utils/create_make_profiler.sh
@@ -7,7 +7,7 @@ fi
 
 #sudo apt install python3-venv
 
-name = $1
+name=$1
 
 python3 -m venv $name 
-$name install https://github.com/gojuno/make-profiler/archive/master.zip
+$name/bin/pip3 install https://github.com/gojuno/make-profiler/archive/master.zip
